104f0bd8a1a8baa649df46cdd98f3e345bfd010f
Bo Wan <bo.wan.cs@gmail.com>
2018-11-14 01:41:58 +1100

    component works

---
+++ src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Problem } from '../../models/problem.model';
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css']
+})
+export class ProblemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
